e8c7882192a868dcc118c458eb29a7cd0a335898
cohlerCoded <jeffcohler@cohlercoded.com>
2021-03-20 12:34:06 -0700

    signed cookies

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cookieParser = require("cookie-parser");
 const app = express();
 
-app.use(cookieParser());
+app.use(cookieParser("thisismysecret"));
 
 app.get("/greet", (req, res) => {
   const { name = "User" } = req.cookies;
@@ -12,6 +12,20 @@ app.get("/greet", (req, res) => {
 app.get("/setname", (req, res) => {
   res.cookie("name", "Jeff");
   res.send("Enjoy your cookie");
+});
+
+app.get("/getsignedcookie", (req, res) => {
+  res.cookie("fruit", "grapes", { signed: true });
+  res.send("OK SIGNED FRUIT COOKIE");
+  // const { fruit = "Apple" } = req.signedCookies;
+  // res.send(`My favorite fruit is ${fruit}!`);
+});
+
+app.get("/verifyfruit", (req, res) => {
+  // console.log(req.cookie);
+  // res.send(req.cookies);
+  console.log(req.signedCookie);
+  res.send(req.signedCookies);
 });
 
 app.listen(3030, () => {
