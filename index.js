09bf7cb3f5cb7a5b0c84af8f5c29a4da49b9bb93
cohlerCoded <jeffcohler@cohlercoded.com>
2021-03-19 23:54:06 -0700

    my first cookie

--- index.js
+++ index.js
@@ -5,6 +5,11 @@ app.get("/greet", (req, res) => {
   res.send("Hey there!");
 });
 
+app.get("/setname", (req, res) => {
+  res.cookie("name", "Jeff");
+  res.send("Enjoy your cookie");
+});
+
 app.listen(3030, () => {
   console.log("listening on localhost 3030");
 });
